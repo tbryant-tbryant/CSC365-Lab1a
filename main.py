6e0e99384812b51a2dd56eeeb166028c1a89f6d1
tbryant-tbryant <tbryant.tbryant@gmail.com>
2021-09-20 11:51:24 -0700

    Loop prompt when incorrect input

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ def main():
         f = open("students.txt", "r")
     except(FileNotFoundError):
         print("No file 'students.txt' found.")
-    prompt()
 
+def initiatePrompt():
+    req = prompt()
+    while (req is not "S" or "T" or "B"):
+        req = prompt()
 
 def prompt():
     print('''Commands:
@@ -16,6 +19,7 @@ def prompt():
         I[nfo]
         Q[uit]''')
     request = input("Request: ")
+    return request
 
 if __name__ == '__main__':
     main()
